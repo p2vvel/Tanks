59fcb340e1f4cc2dd218f0a778aed97bb083702e
pawel <p2vvel@gmail.com>
2020-11-05 00:35:21 +0100

    Added sprites and storage class

--- Tanks/main.cpp
+++ Tanks/main.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
-
+#include<SFML/Graphics.hpp>
 
 
 int main() {
-	std::cout << "XD\n";
+	sf::RenderWindow win(sf::VideoMode(1280, 720), "Tanks LPG");
+	win.setFramerateLimit(60);
+
+	while (win.isOpen()) {
+		sf::Event ev;
+		if (win.pollEvent(ev)) {
+			if (ev.type == sf::Event::Closed)
+				win.close();
+		}
+
+		win.clear(sf::Color(13, 23, 36));
+		win.display();
+	}
 
 	return 0;
 }
