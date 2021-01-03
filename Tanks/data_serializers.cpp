29185bd930e5d1a6bd8f597cfdf7336aacc6047a
p2vvel <p2vvel@gmail.com>
2021-01-03 02:23:14 +0100

    Color data serializers working

--- Tanks/data_serializers.cpp
+++ Tanks/data_serializers.cpp
@@ -15,6 +15,7 @@ void to_json(json& j, const Tank_barrel& barrel) {
 			{"y", barrel.position.x}
 		}},
 		{"angle", barrel.angle},
+		{"size", Names::size_to_string(barrel.my_size)},
 	};
 };
 
@@ -22,6 +23,7 @@ void to_json(json& j, const Tank_barrel& barrel) {
 void from_json(const json& j, Tank_barrel& barrel) {
 	barrel.position = sf::Vector2f(j["pos"]["x"], j["pos"]["y"]);
 	barrel.angle = j["angle"];
+	barrel.my_size = Names::size_to_enum(j["size"]);
 };
 
 
@@ -36,6 +38,7 @@ void to_json(nlohmann::json& j, const Tank& tank) {
 			{"y", tank.position.y},
 		}},
 		{"barrel", *(tank.barrel)},
+		{"color", Names::color_to_string(tank.my_color)},
 	};
 };
 
@@ -46,5 +49,6 @@ void from_json(const nlohmann::json& j, Tank& tank) {
 	tank.set_position(sf::Vector2f(j["pos"]["x"], j["pos"]["y"]));
 	tank.set_angle(j["angle"]);
 	//tank.position = sf::Vector2f(j["pos"]["x"], j["pos"]["y"]);
+	tank.my_color = Names::color_to_enum(j["color"]);
 	from_json(j["barrel"], *(tank.barrel));
 };
--- Tanks/names.h
+++ Tanks/names.h
@@ -2,12 +2,54 @@
 #include<string>
 
 namespace Names {
-	enum tank_color { blue, dark, green, red, sand };
-	enum barrel_size { big, medium, small };
+	enum class tank_color { blue, dark, green, red, sand };
+	enum class barrel_size { big, medium, small };
 
-	tank_color color_to_enum(const std::string& str) {
-		tank_color color;
-		switch (str) {
-		}
+	inline Names::tank_color color_to_enum(const std::string& str) {
+		if (str == "blue")
+			return Names::tank_color::blue;
+		else if (str == "dark")
+			return Names::tank_color::dark;
+		else if (str == "red")
+			return Names::tank_color::red;
+		else if (str == "sand")
+			return Names::tank_color::sand;
+		else// if (str == "green")
+			return Names::tank_color::green;
+	};
+
+	inline std::string color_to_string(const Names::tank_color& color) {
+		switch (color) {
+		case  Names::tank_color::blue:
+			return "blue";
+		case  Names::tank_color::dark:
+			return "dark";
+		case  Names::tank_color::red:
+			return "red";
+		case  Names::tank_color::sand:
+			return "sand";
+		default:	//case  Names::tank_color::green:
+			return "green";
+		};
 	}
-}
+
+	inline Names::barrel_size size_to_enum(const std::string& str) {
+		if (str == "small")
+			return Names::barrel_size::small;
+		else if (str == "medium")
+			return Names::barrel_size::medium;
+		else// if (str == "big")
+			return Names::barrel_size::big;
+	}
+
+	inline std::string size_to_string(const Names::barrel_size& size) {
+		switch (size) {
+		case  Names::barrel_size::small:
+			return "small";
+		case  Names::barrel_size::medium:
+			return "medium";
+		default:	//		case  Names::barrel_size::big
+			return "big";
+		};
+	}
+};
